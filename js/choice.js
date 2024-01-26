251064489c42dcb22baf684110cc43467928b952
PatrikFejes <survivserver@gmail.com>
2024-01-26 20:37:44 +0100

    Clone of my other repo at yeah124

---
+++ js/choice.js
@@ -0,0 +1,52 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+'use strict';(function(){(function(){var cmpScriptElement=document.createElement('script');var firstScript=document.getElementsByTagName('script')[0];cmpScriptElement.async=true;cmpScriptElement.type='text/javascript';var cmpVersion='https://web.archive.org/web/20220310223204/https://quantcast.mgr.consensu.org/tcfv2/cmp2.js?referer=surviv.io';cmpScriptElement.src=cmpVersion;window._qevents=window._qevents||[];(function(){var elem=document.createElement('script');elem.src=(document.location.protocol=="https:"?"https://web.archive.org/web/20220310223204/https://secure":"https://web.archive.org/web/20220310223204/http://edge")+".quantserve.com/quant.js";elem.async=true;elem.type="text/javascript";var scpt=document.getElementsByTagName('script')[0];scpt.parentNode.insertBefore(elem,scpt);})();var qcaccount='p-'+'Zww5m4Azy9gN8';window._qevents.push({qacct:qcaccount,source:"choice"});firstScript.parentNode.insertBefore(cmpScriptElement,firstScript);})();(function(){var css=""
++" .qc-cmp-button.qc-cmp-secondary-button:hover { "
++"   background-color: #368bd6 !important; "
++"   border-color: transparent !important; "
++" } "
++" .qc-cmp-button.qc-cmp-secondary-button:hover { "
++"   color: #ffffff !important; "
++" } "
++" .qc-cmp-button.qc-cmp-secondary-button { "
++"   color: #368bd6 !important; "
++" } "
++" .qc-cmp-button.qc-cmp-secondary-button { "
++"   background-color: #eee !important; "
++"   border-color: transparent !important; "
++" } "
++""
++"";var stylesElement=document.createElement('style');var re=new RegExp('&quote;','g');css=css.replace(re,'"');stylesElement.type='text/css';if(stylesElement.styleSheet){stylesElement.styleSheet.cssText=css;}else{stylesElement.appendChild(document.createTextNode(css));}
+var head=document.head||document.getElementsByTagName('head')[0];head.appendChild(stylesElement);})();var autoDetectedLanguage='en';function splitLang(lang){return lang.length>2?lang.split('-')[0]:lang;};function isSupported(lang){var langs=['en','fr','de','it','es','da','nl','el','hu','pt','ro','fi','pl','sk','sv','no','ru','bg','ca','cs','et','hr','lt','lv','mt','sl','tr','zh'];return langs.indexOf(lang)===-1?false:true;};if(isSupported(splitLang(document.documentElement.lang))){autoDetectedLanguage=splitLang(document.documentElement.lang);}else if(isSupported(splitLang(navigator.language))){autoDetectedLanguage=splitLang(navigator.language);};var choiceMilliSeconds=(new Date).getTime();window.__tcfapi('init',2,function(){},{'premiumProperties':{},'coreUiLabels':{},'premiumUiLabels':{},'theme':{},'coreConfig':{'consentScope':'service','thirdPartyStorageType':'iframe','consentIdentityEnabled':false,'initScreenBodyTextOption':1,'consentOnSafari':false,'lang_':'en','displayUi':'inEU','publisherConsentRestrictionIds':[],'publisherLIRestrictionIds':[],'softOptInEnabled':false,'showSummaryView':true,'persistentConsentLinkLocation':3,'uiLayout':'popup','publisherPurposeIds':[],'publisherPurposeLegitimateInterestIds':[],'publisherSpecialPurposesIds':[],'publisherFeaturesIds':[],'publisherSpecialFeaturesIds':[],'stacks':[1,42],'vendorListUpdateFreq':30,'quantcastAccountId':'Zww5m4Azy9gN8','privacyMode':["GDPR","USP"],'hashCode':'mZL8n0t1lu2jQ06ncu2AOA','publisherCountryCode':'US','vendorPurposeIds':[1,2,3,4,5,6,7,8,9,10],'vendorFeaturesIds':[1,2,3],'vendorPurposeLegitimateInterestIds':[2,3,4,5,6,7,8,9,10],'vendorSpecialFeaturesIds':[1,2],'vendorSpecialPurposesIds':[1,2],'googleEnabled':false,'uspVersion':1,'uspJurisdiction':["CA"],'uspLspact':'N',},'nonIabVendorsInfo':{}});})();
+
+}
+/*
+     FILE ARCHIVED ON 22:32:04 Mar 10, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:01:10 Oct 15, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1377.015
+  exclusion.robots: 0.071
+  exclusion.robots.policy: 0.063
+  cdx.remote: 0.054
+  esindex: 0.009
+  LoadShardBlock: 1345.897 (3)
+  PetaboxLoader3.datanode: 641.23 (4)
+  PetaboxLoader3.resolve: 762.102 (2)
+  load_resource: 61.666
+*/
