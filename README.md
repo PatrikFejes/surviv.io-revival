e727e4a58fb915e4f88971803fb5a69512907568
PatrikFejes <survivserver@gmail.com>
2024-01-26 23:27:56 +0100

    Update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# surviv.io-revival
+# survivio-recreation-attempt
 
-This is to test if I configured it properly
+This is my attempt at reviving surviv.io, but since I'm an amateur this is quite impossible to me, So here is my progress so far, in this repository I upload my collected resources so far. I arranged the folders the way I did, because it represents the structure of the game's resources as the website.
+Feel free to clone this repo and complete it with the missing resources, and produce a working result. All I want to achieve is to be able to play the game, so I need some support to get it all done.
---
+++ js/manifest.1deda88d.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(n){function r(r){for(var t,f,i=r[0],c=r[1],a=r[2],v=0,b=[];v<i.length;v++)f=i[v],Object.prototype.hasOwnProperty.call(u,f)&&u[f]&&b.push(u[f][0]),u[f]=0;for(t in c)Object.prototype.hasOwnProperty.call(c,t)&&(n[t]=c[t]);for(l&&l(r);b.length;)b.shift()();return o.push.apply(o,a||[]),e()}function e(){for(var n,r=0;r<o.length;r++){for(var e=o[r],t=!0,i=1;i<e.length;i++){var c=e[i];0!==u[c]&&(t=!1)}t&&(o.splice(r--,1),n=f(f.s=e[0]))}return n}var t={},u={1:0},o=[];function f(r){if(t[r])return t[r].exports;var e=t[r]={i:r,l:!1,exports:{}};return n[r].call(e.exports,e,e.exports,f),e.l=!0,e.exports}f.m=n,f.c=t,f.d=function(n,r,e){f.o(n,r)||Object.defineProperty(n,r,{enumerable:!0,get:e})},f.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},f.t=function(n,r){if(1&r&&(n=f(n)),8&r)return n;if(4&r&&"object"==typeof n&&n&&n.__esModule)return n;var e=Object.create(null);if(f.r(e),Object.defineProperty(e,"default",{enumerable:!0,value:n}),2&r&&"string"!=typeof n)for(var t in n)f.d(e,t,function(r){return n[r]}.bind(null,t));return e},f.n=function(n){var r=n&&n.__esModule?function(){return n.default}:function(){return n};return f.d(r,"a",r),r},f.o=function(n,r){return Object.prototype.hasOwnProperty.call(n,r)},f.p="";var i=window.webpackJsonp=window.webpackJsonp||[],c=i.push.bind(i);i.push=r,i=i.slice();for(var a=0;a<i.length;a++)r(i[a]);var l=c;e()}([]);
+//# sourceMappingURL=manifest.1deda88d.js.map
+
+}
+/*
+     FILE ARCHIVED ON 19:10:25 Feb 08, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:07:40 Oct 15, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 107.689
+  exclusion.robots: 0.092
+  exclusion.robots.policy: 0.081
+  cdx.remote: 0.07
+  esindex: 0.01
+  LoadShardBlock: 64.213 (3)
+  PetaboxLoader3.datanode: 70.15 (5)
+  load_resource: 118.705 (2)
+  PetaboxLoader3.resolve: 88.738 (2)
+*/
